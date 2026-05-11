0f929d0afb8489c1a7ca68ed5093607a9e2cb41c
ngohung55235 <ngohung55235@gmail.com>
2026-05-11 11:02:11 +0700

    lab01

---
+++ lab-01/ex02/ex02_08.py
@@ -0,0 +1,8 @@
+def chia_het_cho_5(so_nhi_phan):
+    so_thap_phan = int(so_nhi_phan, 2)
+    if so_thap_phan % 5 == 0:
+        return True
+    else:
+        return False
+
+chuoi_so_nhi_phan = input("Nhập chuỗi số nhị phân (phân tách bởi dấu phẩy): ")
